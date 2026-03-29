ef3a0862a596a5230616493e1e44d4934d2d14c3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2026-03-30 00:22:01 +0530

    working out with gemini

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
 
-if __name__ == "__main__":
-    main()
+client = genai.Client(api_key=api_key)
+prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=prompt,
+)
+print(f"User prompt: {prompt}")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(f"Response: {response}")
