79087bb61168dba179ea6c2c489f735c259f2e3e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2026-03-30 01:43:21 +0530

    run python file tool

---
+++ functions/run_python_file.py
@@ -0,0 +1,58 @@
+import os
+import subprocess
+from pydantic import BaseModel
+
+
+class PythonResult(BaseModel):
+    stdout: str
+    stderr: str
+    return_code: int
+
+
+def run_python_file(
+    working_directory: str, file_path: str, args: list[str] | None = None
+) -> str:
+    abs_working_directory: str = os.path.abspath(working_directory)
+    abs_file_path: str = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_directory):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: "{file_path}" does not exist or is not a regular file'
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file'
+
+    try:
+        command: list[str] = ["python", abs_file_path]
+        if args:
+            command.extend(args)
+
+        result: subprocess.CompletedProcess[str] = subprocess.run(
+            command,
+            cwd=abs_working_directory,
+            capture_output=True,
+            text=True,
+            timeout=30,
+        )
+
+        parsed: PythonResult = PythonResult(
+            stdout=result.stdout,
+            stderr=result.stderr,
+            return_code=result.returncode,
+        )
+
+        output: str = ""
+        if parsed.return_code != 0:
+            output += f"Process exited with code {parsed.return_code}\n"
+
+        if not parsed.stdout and not parsed.stderr:
+            output += "No output produced"
+        else:
+            if parsed.stdout:
+                output += f"STDOUT:\n{parsed.stdout}"
+            if parsed.stderr:
+                output += f"STDERR:\n{parsed.stderr}"
+
+        return output
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
